dc939f95a6d3e4716cab9b44d0bcb2608df75a98
tyrawalker <tyra.walker@bryanuniversity.edu>
2021-03-27 16:29:40 -0400

    week1 v2

---
+++ Week_1/PnPNodeBasicsAndPatterns/app.js
@@ -0,0 +1,7 @@
+ const calculate = require("./math")
+ 
+ console.log(calculate.add(2,3)); 
+ console.log(calculate.sub(4,3));
+ console.log(calculate.mul(5,6));
+ console.log(calculate.div(10,2))
+ 
